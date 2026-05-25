0b007b8b85fcb0d15c6b32cc13412c9855202a2d
drjstancer <drj.stancer@gmail.com>
2026-05-25 17:50:00 -0500

    build: add structured qr discovery routing map

---
+++ content/qr/qrDiscoveryMap.js
@@ -0,0 +1,5 @@
+export const qrDiscoveryMap = {
+  'meddetectives://qr/jordan-timeline': 'qr-01',
+  'meddetectives://qr/jordan-journal': 'qr-02',
+  'meddetectives://qr/jordan-escalation': 'pin-01'
+};
